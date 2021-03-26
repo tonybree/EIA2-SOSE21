675df58128ef8ee462ccc33b1a809ab7ef2113a6
Tony Hild <tony.hild@hs-furtwangen.de>
2021-03-26 20:10:22 +0100

    L01_Poem
    Array Output

--- L01_Poem/Poem.js
+++ L01_Poem/Poem.js
@@ -1,3 +1,11 @@
 "use strict";
-console.log("Expecto Patronum");
+var Zufallsgedicht;
+(function (Zufallsgedicht) {
+    let subjekt = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let praedikat = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objekt = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    console.log(subjekt);
+    console.log(praedikat);
+    console.log(objekt);
+})(Zufallsgedicht || (Zufallsgedicht = {}));
 //# sourceMappingURL=Poem.js.map
--- L01_Poem/Poem.ts
+++ L01_Poem/Poem.ts
@@ -1,1 +1,15 @@
-console.log("Expecto Patronum");
+namespace Zufallsgedicht{
+
+
+let subjekt: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+let praedikat: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+let objekt: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+
+
+
+
+console.log(subjekt);
+console.log(praedikat);
+console.log(objekt);
+
+}
