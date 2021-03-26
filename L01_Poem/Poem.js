7b5dde969de3e0483efac21e1e6aaa70826af1d9
Tony Hild <tony.hild@hs-furtwangen.de>
2021-03-26 21:04:04 +0100

    ausgabe test

--- L01_Poem/Poem.js
+++ L01_Poem/Poem.js
@@ -5,11 +5,16 @@ let objekte = [];
 //let subjektStrings: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
 //let praedikatStrings: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
 //let objektStrings: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+/*
 function initialisieren() {
-    let harry = new Subjekt();
+    let harry: Subjekt = new Subjekt();
     harry.wort = "Harry";
     subjekte[subjekte.length] = harry;
 }
+
 initialisieren();
+
 console.log(subjekte[0].wort);
+*/
+console.log("wort");
 //# sourceMappingURL=Poem.js.map
